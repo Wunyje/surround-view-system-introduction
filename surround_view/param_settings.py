6a9e4004abdbe2b1331617ade17cf710c81bf5a7
Wunyje <wunyje@gmail.com>
2024-01-23 17:33:34 +0800

    done first para change

--- surround_view/param_settings.py
+++ surround_view/param_settings.py
@@ -7,23 +7,23 @@ camera_names = ["front", "back", "left", "right"]
 # --------------------------------------------------------------------
 # (shift_width, shift_height): how far away the birdview looks outside
 # of the calibration pattern in horizontal and vertical directions
-shift_w = 300
-shift_h = 300
+shift_w = 100
+shift_h = 150
 
 # size of the gap between the calibration pattern and the car
 # in horizontal and vertical directions
-inn_shift_w = 20
-inn_shift_h = 50
+inn_shift_w = 0
+inn_shift_h = 0
 
 # total width/height of the stitched image
-total_w = 600 + 2 * shift_w
-total_h = 1000 + 2 * shift_h
+total_w = 210 + 2 * shift_w
+total_h = 300 + 2 * shift_h
 
 # four corners of the rectangular region occupied by the car
 # top-left (x_left, y_top), bottom-right (x_right, y_bottom)
-xl = shift_w + 180 + inn_shift_w
+xl = shift_w + 50 + inn_shift_w
 xr = total_w - xl
-yt = shift_h + 200 + inn_shift_h
+yt = shift_h + 100 + inn_shift_h
 yb = total_h - yt
 # --------------------------------------------------------------------
 
@@ -38,25 +38,25 @@ project_shapes = {
 # you must click these pixels in the same order when running
 # the get_projection_map.py script
 project_keypoints = {
-    "front": [(shift_w + 120, shift_h),
-              (shift_w + 480, shift_h),
-              (shift_w + 120, shift_h + 160),
-              (shift_w + 480, shift_h + 160)],
+    "front": [(shift_w + 40, shift_h +  74),
+              (shift_w + 100, shift_h +  74),
+              (shift_w + 40, shift_h + 111),
+              (shift_w + 100, shift_h + 111)],
 
-    "back":  [(shift_w + 120, shift_h),
-              (shift_w + 480, shift_h),
-              (shift_w + 120, shift_h + 160),
-              (shift_w + 480, shift_h + 160)],
+    "back":  [(shift_w + 40, shift_h +  74),
+              (shift_w + 100, shift_h +  74),
+              (shift_w + 40, shift_h + 111),
+              (shift_w + 100, shift_h + 111)],
 
-    "left":  [(shift_h + 280, shift_w),
-              (shift_h + 840, shift_w),
-              (shift_h + 280, shift_w + 160),
-              (shift_h + 840, shift_w + 160)],
+    "left":  [(shift_h + 164, shift_w),
+              (shift_h + 124, shift_w),
+              (shift_h + 164, shift_w + 37),
+              (shift_h + 124, shift_w + 37)],
 
-    "right": [(shift_h + 160, shift_w),
-              (shift_h + 720, shift_w),
-              (shift_h + 160, shift_w + 160),
-              (shift_h + 720, shift_w + 160)]
+    "right": [(shift_h + 124, shift_w),
+              (shift_h + 164, shift_w),
+              (shift_h + 124, shift_w + 37),
+              (shift_h + 164, shift_w + 37)]
 }
 
 current_file_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
