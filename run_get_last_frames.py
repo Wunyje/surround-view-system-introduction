1ebc9fd80be519fe4dbb32060d03966c58c5a69f
rk3588_dev <wunyje@gmail.com>
2024-01-24 16:02:54 +0800

    update imgs, have new try

--- run_get_last_frames.py
+++ run_get_last_frames.py
@@ -2,13 +2,19 @@ import cv2
 import os
 '''
 gst-launch-1.0 -e \
+    v4l2src device=/dev/video44 ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! queue  ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=960 sink_1::ypos=0 sink_2::xpos=0 sink_2::ypos=640 sink_3::xpos=960 sink_3::ypos=640 ! xvimagesink  sync=false \
+    v4l2src device=/dev/video53  ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! queue ! videoflip method=rotate-180 ! mix. \
+    v4l2src device=/dev/video62  ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! queue ! videoflip method=rotate-180 ! mix. \
+    v4l2src device=/dev/video71  ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! queue ! mix. 
+    
+gst-launch-1.0 -e \
     v4l2src device=/dev/video44 ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! tee name=video44_t \
     video44_t. ! queue  ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=960 sink_1::ypos=0 sink_2::xpos=0 sink_2::ypos=640 sink_3::xpos=960 sink_3::ypos=640 ! xvimagesink  sync=false \
     video44_t. !  mpph264enc ! h264parse ! mp4mux ! filesink location=video44.mp4 \
-    v4l2src device=/dev/video53 ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! tee name=video53_t \
+    v4l2src device=/dev/video53 ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! videoflip method=rotate-180 ! tee name=video53_t \
     video53_t. ! queue ! mix. \
     video53_t. !  mpph264enc ! h264parse ! mp4mux ! filesink location=video53.mp4 \
-    v4l2src device=/dev/video62 ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! tee name=video62_t \
+    v4l2src device=/dev/video62 ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! videoflip method=rotate-180 ! tee name=video62_t \
     video62_t. ! queue ! mix. \
     video62_t. !  mpph264enc ! h264parse ! mp4mux ! filesink location=video62.mp4 \
     v4l2src device=/dev/video71 ! video/x-raw,format=NV12,width=960,height=640,framerate=30/1 ! tee name=video71_t \
@@ -39,8 +45,6 @@ for video, output_image in zip(videos, output_images):
     ret, frame = cap.read()
 
     if ret:
-        if output_image in ['left.png', 'back.png']:
-            frame = cv2.rotate(frame, cv2.ROTATE_180)
         # Save the last frame to a file
         cv2.imwrite(output_image, frame)
     else:
